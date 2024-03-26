d32a9c144166344875b47f287c282bbcb3d28cdc
TamNhien <canhthien91@gmail.com>
2024-03-26 19:28:46 +0700

    Add project files.

---
+++ P_DemTuVaKiTu.py
@@ -0,0 +1,7 @@
+﻿user_input = input("Nhập một câu bất kỳ : ")
+
+word_count = len(user_input.split())
+char_count = len(user_input)
+
+print("Số từ trong câu là : ", word_count)
+print("Số ký tự trong câu là : ", char_count)
